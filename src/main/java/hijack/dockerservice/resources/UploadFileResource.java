58a2d92632173f0a1762d9dd46c3e3dd18b0ba0d
chunjie-wang <chunjie.wangcn@gmail.com>
2015-08-08 16:07:46 +0800

    Test--Add multiple files upload support

--- src/main/java/hijack/dockerservice/resources/UploadFileResource.java
+++ src/main/java/hijack/dockerservice/resources/UploadFileResource.java
@@ -1,6 +1,9 @@
 package hijack.dockerservice.resources;
 
 import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.BodyPart;
+import com.sun.jersey.multipart.BodyPartEntity;
+import com.sun.jersey.multipart.FormDataMultiPart;
 import com.sun.jersey.multipart.FormDataParam;
 import hijack.dockerservice.util.FileUtils;
 import org.slf4j.Logger;
@@ -10,6 +13,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.*;
+import java.text.ParseException;
 
 /**
  * Created by lovefly1983 on 5/8/15.
@@ -17,19 +21,35 @@ import java.io.*;
 @Path("/comp/file")
 public class UploadFileResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadFileResource.class);
+    private static final String CONTENT_DISPOSITION = "Content-Disposition";
+    private static final String FILE_NAME = "filename";
+    private static final String NEWLINE = System.getProperty("line.separator");
+
     @POST
     @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
-                               @FormDataParam("file") FormDataContentDisposition fileDetail) {
+    public Response uploadFile(FormDataMultiPart f) throws ParseException {
 
         LOGGER.info("upload file ... {}.", System.getProperty("user.dir"));
-        String uploadedFileLocation = "../images/" + fileDetail.getFileName();
 
-        // save it
-        FileUtils.writeToFile(uploadedInputStream, uploadedFileLocation);
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+            for (BodyPart bp : f.getBodyParts()) {
+                System.out.println(bp.getMediaType());
+                System.out.println(bp.getHeaders().get(CONTENT_DISPOSITION));
 
-        String output = "File uploaded to : " + uploadedFileLocation;
-        return Response.status(200).entity(output).build();
+                String fileName = bp.getParameterizedHeaders().getFirst(CONTENT_DISPOSITION).getParameters().get(FILE_NAME);
+                BodyPartEntity bodyPartEntity = (BodyPartEntity) bp.getEntity();
+
+                String uploadedFileLocation = "../images/" + fileName;
+
+                FileUtils.writeToFile(bodyPartEntity.getInputStream(), uploadedFileLocation);
+                stringBuffer.append("File uploaded to : ").append(uploadedFileLocation).append(NEWLINE);
+                bp.cleanup();
+            }
+        } finally {
+            f.cleanup();
+        }
+        return Response.status(200).entity(stringBuffer.toString()).build();
     }
 }
