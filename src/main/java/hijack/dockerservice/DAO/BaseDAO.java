cb1c565aeeb3fea6196d4c61ddb10b17607e170e
chunjie-wang <chunjie.wangcn@gmail.com>
2015-08-15 19:45:27 +0800

    1. Upload the images using ajax rather than default form action (whole page refresh) 2. Enable upload filename with Chinese characters

--- src/main/java/hijack/dockerservice/DAO/BaseDAO.java
+++ src/main/java/hijack/dockerservice/DAO/BaseDAO.java
@@ -8,4 +8,6 @@ public interface BaseDAO {
      * close with no args is used to close the connection
      */
     void close();
+
+    public static final String URL = "%s?user=%s&password=%s&useUnicode=true&characterEncoding=UTF8";
 }
--- src/main/java/hijack/dockerservice/resources/FileResource.java
+++ src/main/java/hijack/dockerservice/resources/FileResource.java
@@ -3,10 +3,13 @@ package hijack.dockerservice.resources;
 import com.sun.jersey.multipart.BodyPart;
 import com.sun.jersey.multipart.BodyPartEntity;
 import com.sun.jersey.multipart.FormDataMultiPart;
+import hijack.dockerservice.DAO.BaseDAO;
 import hijack.dockerservice.DAO.ImageDAO;
 import hijack.dockerservice.DockerServiceMainConfiguration;
 import hijack.dockerservice.model.FileQueryResponse;
 import hijack.dockerservice.util.FileUtils;
+import io.dropwizard.db.DataSourceFactory;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,6 +17,8 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.sql.*;
 import java.text.ParseException;
 
 /**
@@ -24,7 +29,7 @@ public class FileResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(FileResource.class);
     private static final String CONTENT_DISPOSITION = "Content-Disposition";
     private static final String FILE_NAME = "filename";
-    private static final String NEWLINE = System.getProperty("line.separator");
+    private static final String NEW_LINE = System.getProperty("line.separator");
     private ImageDAO imageDAO;
     private DockerServiceMainConfiguration configuration;
 
@@ -36,35 +41,31 @@ public class FileResource {
     @POST
     @Path("/upload")
     @Consumes(MediaType.MULTIPART_FORM_DATA)
-    public Response uploadFile(FormDataMultiPart f) throws ParseException {
+    public void uploadFile(FormDataMultiPart f) throws ParseException, UnsupportedEncodingException {
 
         LOGGER.info("upload file ... {}.", configuration.getImagesFolder());
 
-        boolean succeed = false;
         try {
             for (BodyPart bp : f.getBodyParts()) {
-                System.out.println(bp.getMediaType());
-                System.out.println(bp.getHeaders().get(CONTENT_DISPOSITION));
+                String str = bp.getParameterizedHeaders().getFirst(CONTENT_DISPOSITION).getParameters().get(FILE_NAME);
 
-                String fileName = bp.getParameterizedHeaders().getFirst(CONTENT_DISPOSITION).getParameters().get(FILE_NAME);
+                // Make sure we can get the file name correctly
+                String fileName = new String(str.getBytes("iso8859-1"), "utf-8");
+
+                LOGGER.info("file name {}", fileName);
+
                 BodyPartEntity bodyPartEntity = (BodyPartEntity) bp.getEntity();
 
-                // Write to the file system
-                FileUtils.writeToFile(bodyPartEntity.getInputStream(), configuration.getImagesFolder() + File.separator+ fileName);
+                if (StringUtils.isNotEmpty(fileName)) {
+                    // Write to the file system
+                    FileUtils.writeToFile(bodyPartEntity.getInputStream(), configuration.getImagesFolder() + File.separator+ fileName);
 
-                imageDAO.insert(4, configuration.getImagesVirtualFolder() + File.separator+ fileName);
-                bp.cleanup();
-                succeed = true;
+                    imageDAO.insert(4, configuration.getImagesVirtualFolder() + File.separator + fileName);
+                    bp.cleanup();
+                }
             }
         } finally {
             f.cleanup();
-        }
-
-        //
-        if (succeed) {
-            return Response.status(200).entity("File upload succeeds...").build();
-        } else {
-            return Response.status(400).entity("File upload fails...").build();
         }
     }
 
